030ac0290eedce867d285c936208569c0249d2f7
Grace Hawkins <amoomajid99@gmail.com>
2021-08-15 19:32:24 +0430

    Init project

---
+++ api.php
@@ -0,0 +1,71 @@
+<?php
+
+require_once('settings.php');
+
+$BASE_URL = 'http://' . $SEPIDAR_ADDRESS . ':' . $SEPIDAR_PORT .
+            '/api/v1';
+
+$REQUEST_HEADERS = array(
+    'Authorization' => 'Basic ' . base64_encode($SEPIDAR_USERNAME . ':' . $SEPIDAR_PASSWORD)
+);
+
+
+function get_url($method_name) {
+    global $BASE_URL;
+    return $BASE_URL . '/' . $method_name;
+}
+
+function sw_filter_is_online_market($product) {
+    return $product['stockcode'] == 1;
+}
+
+function sw_filter_is_online_shop($product) {
+    return $product['saleTypeNumber'] == 2;
+}
+
+function fetch_all_sepidar_products_quantity() {
+    global $REQUEST_HEADERS;
+    global $QUANTITY_FIELD_NAME, $SKU_FIELD_NAME;
+    $url = get_url('StockSummary');
+    $args = array(
+        'headers' => $REQUEST_HEADERS,
+        'timeout' => 20,
+    );
+    $req = wp_remote_get($url, $args);
+    if (is_wp_error($req)) {
+        error_log('Failed to fetch quantities');
+        error_log(print_r($req->errors, true));
+        return array();
+    }
+
+    $body = wp_remote_retrieve_body($req);
+    $products = json_decode($body, true);
+    $products = array_filter($products, "sw_filter_is_online_market");
+    $result = array();
+    foreach ($products as $product)
+        $result[$product['itemcode']] = $product['quantity'];
+    return $result;
+}
+
+function fetch_all_sepidar_products_price() {
+    global $REQUEST_HEADERS;
+    $url = get_url('PriceNoteList');
+    $args = array(
+        'headers' => $REQUEST_HEADERS,
+        'timeout' => 20,
+    );
+    $req = wp_remote_get($url, $args);
+    if (is_wp_error($req)) {
+        error_log('Failed to fetch prices');
+        error_log(print_r($req->errors, true));
+        return array();
+    }
+
+    $body = wp_remote_retrieve_body($req);
+    $products = json_decode($body, true);
+    $products = array_filter($products, "sw_filter_is_online_shop");
+    $result = array();
+    foreach($products as $product)
+        $result[$product['itemCode']] = $product['fee'];
+    return $result;
+}
---
+++ sepidar-woocommerce.php
@@ -0,0 +1,70 @@
+<?php
+/**
+ * Plugin Name:     Sepidar Woocommerce
+ * Plugin URI:      https://github.com/Grackins/SepidarWoocommerce
+ * Description:     Wordpress plugin to sync WooCommerce and Sepidar
+ * Author:          Grace Hawkins, Alireza Rahmani
+ * Author URI:      https://github.com/GarceHawkins
+ * Text Domain:     sepidar-woocommerce
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Sepidar_Woocommerce
+ */
+
+require_once('settings.php');
+require_once('api.php');
+
+error_log("Running the shit\n");
+
+add_action('sw_cron_update_quantity', 'update_quantity_data');
+add_action('sw_cron_update_price', 'update_price_data');
+add_filter('cron_schedules', 'sw_add_cron_interval');
+
+if (! wp_next_scheduled('sw_cron_update_price')) {
+    wp_schedule_event(time(), 'update_price_interval', 'sw_cron_update_price');
+}
+if (! wp_next_scheduled('sw_cron_update_quantity')) {
+    wp_schedule_event(time(), 'update_quantity_interval', 'sw_cron_update_quantity');
+}
+
+function sw_add_cron_interval( $schedules ) { 
+    global $SW_QUANTITY_UPDATE_INTERVAL, $SW_PRICE_UPDATE_INTERVAL;
+    $quantity_msg = 'Every ' . $SW_QUANTITY_UPDATE_INTERVAL . ' Secs';
+    $price_msg = 'Every ' . $SW_PRICE_UPDATE_INTERVAL . ' Secs';
+    $schedules['update_quantity_interval'] = array(
+        'interval' => $SW_QUANTITY_UPDATE_INTERVAL,
+        'display'  => esc_html__( $quantity_msg ), );
+    $schedules['update_price_interval'] = array(
+        'interval' => $SW_PRICE_UPDATE_INTERVAL,
+        'display'  => esc_html__( $price_msg ), );
+    return $schedules;
+}
+
+function update_quantity_data() {
+    error_log("Updating quantity data");
+    $sep_quantities_list = fetch_all_sepidar_products_quantity();
+    foreach ($sep_quantities_list as $sku => $quantity) {
+        $product_id = wc_get_product_id_by_sku($sku);
+        if ($product_id == 0)
+            continue;
+        error_log('Found product: ' . $sku);
+        $product = new WC_Product($product_id);
+        $product->set_stock_quantity($quantity);
+        $product->save();
+    }
+}
+
+function update_price_data() {
+    error_log("Updating price data");
+    $sep_prices_list = fetch_all_sepidar_products_price();
+    foreach ($sep_prices_list as $sku => $price) {
+        $product_id = wc_get_product_id_by_sku($sku);
+        if ($product_id == 0)
+            continue;
+        error_log('Found product: ' . $sku);
+        $product = new WC_Product($product_id);
+        $product->set_regular_price($price);
+        $product->save();
+    }
+}
