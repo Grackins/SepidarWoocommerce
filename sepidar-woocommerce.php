e73c3a89b6d985067dba9d2fa1c51f22c2dcdb16
Grace Hawkins <amoomajid99@gmail.com>
2022-02-04 02:40:53 +0330

    Call complete_todo_factors periodically

--- sepidar-woocommerce.php
+++ sepidar-woocommerce.php
@@ -7,7 +7,7 @@
  * Author URI:      https://github.com/GarceHawkins
  * Text Domain:     sepidar-woocommerce
  * Domain Path:     /languages
- * Version:         1.0.2
+ * Version:         1.1.2
  *
  * @package         Sepidar_Woocommerce
  */
@@ -20,6 +20,7 @@ require_once('jobs.php');
 register_activation_hook(__FILE__, 'sw_db_create_database');
 add_action('sw_cron_update_quantity', 'update_quantity_data');
 add_action('sw_cron_update_price', 'update_price_data');
+add_action('sw_cron_save_factors', 'sw_complete_todo_factors');
 add_action('woocommerce_payment_complete', 'sw_payment_complete');
 add_filter('cron_schedules', 'sw_add_cron_interval');
 
@@ -29,17 +30,25 @@ if (! wp_next_scheduled('sw_cron_update_price')) {
 if (! wp_next_scheduled('sw_cron_update_quantity')) {
     wp_schedule_event(time(), 'update_quantity_interval', 'sw_cron_update_quantity');
 }
+if (! wp_next_scheduled('sw_cron_save_factors')) {
+    wp_schedule_event(time(), 'save_factors_interval', 'sw_cron_save_factors');
+}
 
 function sw_add_cron_interval( $schedules ) { 
     global $SW_QUANTITY_UPDATE_INTERVAL, $SW_PRICE_UPDATE_INTERVAL;
+    global $SW_SAVE_FACTORS_INTERVAL;
     $quantity_msg = 'Every ' . $SW_QUANTITY_UPDATE_INTERVAL . ' Secs';
-    $price_msg = 'Every ' . $SW_PRICE_UPDATE_INTERVAL . ' Secs';
     $schedules['update_quantity_interval'] = array(
         'interval' => $SW_QUANTITY_UPDATE_INTERVAL,
         'display'  => esc_html__( $quantity_msg ), );
+    $price_msg = 'Every ' . $SW_PRICE_UPDATE_INTERVAL . ' Secs';
     $schedules['update_price_interval'] = array(
         'interval' => $SW_PRICE_UPDATE_INTERVAL,
         'display'  => esc_html__( $price_msg ), );
+    $factors_msg = 'Every ' . $SW_SAVE_FACTORS_INTERVAL . ' Secs';
+    $schedules['save_factors_interval'] = array(
+        'interval' => $SW_SAVE_FACTORS_INTERVAL,
+        'display'  => esc_html__( $factors_msg ), );
     return $schedules;
 }
 
