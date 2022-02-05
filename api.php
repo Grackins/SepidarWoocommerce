750eb8d7313fd8da54fc4f9f68fe4a71cb772df6
alireza <rezaali1387@gmail.com>
2022-02-05 20:57:14 +0330

    fix sent req

--- api.php
+++ api.php
@@ -76,74 +76,93 @@ function fetch_all_sepidar_products_price() {
 }
 
 function sw_api_register_invoice($order) {
-    global $REQUEST_HEADERS;
-    $data = array();
-    $date_modified = $order->get_date_modified();
-    $date = $date_modified->format('Y-m-d');
-    foreach ($order->get_items() as $item_key => $item ){
-        $itemcode = $item->get_product_id();
-        $quantity = $item->get_quantity();
-        $product = $item->get_product();
-        $price = $product->get_price();
-        $fee = $price * 10000;
-        $item_data = array(
-            "sourceid"=> 0,
-            "customercode"=> "20001",
-            "saletypenumber"=> 2,
-            "discount"=> 0,
-            "addition"=> 0,
-            "currencyRate"=> 1,
-            "stockCode"=> 101,
-            "number" => sw_get_invoice_number($order->get_id()),
-            "date" => $date,
-            "itemcode" => strval($itemcode),
-            "quantity" => $quantity,
-            "fee" => $fee
-        );
-        array_push($data, $item_data);
-    }
-    $args = array(
-        'headers' => $REQUEST_HEADERS,
-        'timeout' => 20,
-        'method'  => 'POST',
-        'body'    => json_encode($data),
-    );
-    $url = get_url('RegisterInvoice');
-    $req = wp_remote_post($url, $args);
-    if (is_wp_error($req)) {
-        error_log('Failed to register invoice');
-        error_log(print_r($req->errors, true));
-        return false;
-    }
-    $body = wp_remote_retrieve_body($req);
-    $result = json_decode($body, true);
-    if (strlen($result['message']) == 0)
-        return true;
-    return false;
+	global $REQUEST_HEADERS;
+	$data = array();
+	$date_modified = $order->get_date_modified();
+	$date = $date_modified->format('Y-m-d');
+	foreach ($order->get_items() as $item_key => $item ){
+		$quantity = $item->get_quantity();
+		//$product = wc_get_product($item);
+		$product = $item->get_product();
+		//$itemcode = $item->get_product_id();
+		$item_sku = $product->get_sku();
+		$price = $product->get_price();
+		$fee = $price * 10000;
+		//error_log($itemcode);
+		//error_log($item_sku);
+		$item_data = array(
+			"sourceid"=> 0,
+			"customercode"=> "20001",
+			"saletypenumber"=> 2,
+			"discount"=> 0,
+			"addition"=> 0,
+			"currencyRate"=> 1,
+			"stockCode"=> 101,
+			"number" => sw_get_invoice_number($order->get_id()),
+			"date" => $date,
+			"itemcode" => strval($item_sku),
+			"quantity" => $quantity,
+			"fee" => $fee
+		);
+		array_push($data, $item_data);
+	}
+	//error_log(print_r(json_encode($data), true));
+	$args = array(
+		'headers' => $REQUEST_HEADERS,
+		'timeout' => 20,
+		'method'  => 'POST',
+		'body'    => json_encode($data),
+		'data_format' => 'body',
+	);
+	$url = get_url('RegisterInvoice');
+	$req = wp_remote_post($url, $args);
+	if (is_wp_error($req)) {
+		error_log('Failed to register invoice');
+		error_log(print_r($req->errors, true));
+		return false;
+	}
+	error_log("alireza1");
+	$body = wp_remote_retrieve_body($req);
+	$result = json_decode($body, true);
+	error_log("alireza2");
+	error_log(wp_remote_retrieve_response_code($req));
+	//error_log();
+	error_log(print_r($result, true));
+	if (wp_remote_retrieve_response_code($req) != 200)
+		return false;
+	error_log("alireza3");
+	if (strlen($result['message']) == 0)
+		return true;
+	return false;
 }
 
 function sw_api_register_delivery($order) {
-    global $REQUEST_HEADERS;
-    $data = array(
-        'invoicenumber' => sw_get_invoice_number($order->get_id()),
-        'saletypenumner' => 2,
-    );
-    $args = array(
-        'headers' => $REQUEST_HEADERS,
-        'timeout' => 20,
-        'method'  => 'POST',
-        'body'    => json_encode($data),
-    );
-    $url = get_url('RegisterInventorydelivery');
-    $req = wp_remote_post($url, $args);
-    if (is_wp_error($req)) {
-        error_log('Failed to register delivery');
-        error_log(print_r($req->errors, true));
-        return false;
-    }
-    $body = wp_remote_retrieve_body($req);
-    $result = json_decode($body, true);
-    if (strlen($result['message']) == 0)
-        return false;
-    return true;
+	global $REQUEST_HEADERS;
+	$data = array(
+		'invoicenumber' => sw_get_invoice_number($order->get_id()),
+		'saletypenumner' => 2,
+	);
+	$args = array(
+		'headers' => $REQUEST_HEADERS,
+		'timeout' => 20,
+		'method'  => 'POST',
+		'body'    => json_encode($data),
+		'data_format' => 'body',
+	);
+	$url = get_url('RegisterInventorydelivery');
+	$req = wp_remote_post($url, $args);
+	if (is_wp_error($req)) {
+		error_log('Failed to register delivery');
+		error_log(print_r($req->errors, true));
+		return false;
+	}
+	$body = wp_remote_retrieve_body($req);
+	$result = json_decode($body, true);
+	error_log(print_r($result, true));
+	error_log(wp_remote_retrieve_response_code($req));
+	if (wp_remote_retrieve_response_code($req) != 200)
+		return false;
+	if (strlen($result['message']) == 0)
+		return false;
+	return true;
 }
