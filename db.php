453f5428e4774ec130f0c11d1fc8504a7a59c200
alireza <rezaali1387@gmail.com>
2022-05-31 13:32:15 +0430

    change factor_numbers to factor_id

--- db.php
+++ db.php
@@ -40,16 +40,16 @@ function sw_db_add_todo_factor($order_id) {
     error_log('Adding todo factor ' . $order_id);
     $table = table_name('todo_factors');
     $factor_number = sw_db_get_last_factor_number();
-    $sql = "INSERT INTO $table (order_id, factor_number, stage) VALUES ($order_id, $factor_number, 0);";
+    $sql = "INSERT INTO $table (order_id, factor_id, stage) VALUES ($order_id, $factor_number, 0);";
     dbDelta($sql);
 }
 
 function sw_db_get_todo_factors() {
     global $wpdb;
     $table = table_name('todo_factors');
-    $orders = $wpdb->get_results("SELECT order_id, factor_number, stage FROM $table");
+    $orders = $wpdb->get_results("SELECT order_id, factor_id, stage FROM $table");
     $orders = array_map(function ($row) {
-	    return new SW_TodoFactor($row->order_id, $row->factor_number, $row->stage);
+	    return new SW_TodoFactor($row->order_id, $row->factor_id, $row->stage);
     }, $orders);
     return $orders;
 }
@@ -71,5 +71,5 @@ function sw_db_get_last_factor_number() {
 	$factor_number = $wpdb->get_results("SELECT factor_id FROM $table ORDER BY factor_id DESC LIMIT 1");
 	if ($factor_number == null)
 		return 1;
-	return $factor_number;
+	return $factor_number->factor_id;
 }
