d09995e74ce9a39c463cc986b21cec7c11bf4b75
alireza <rezaali1387@gmail.com>
2022-05-31 17:21:44 +0430

    Upgrade version

--- sepidar-woocommerce.php
+++ sepidar-woocommerce.php
@@ -7,7 +7,7 @@
  * Author URI:      https://github.com/GarceHawkins
  * Text Domain:     sepidar-woocommerce
  * Domain Path:     /languages
- * Version:         1.1.6
+ * Version:         1.2.0
  *
  * @package         Sepidar_Woocommerce
  */
